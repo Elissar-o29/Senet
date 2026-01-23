a3d9552a716d46161a8ae060cf66ff30169b5970
Lucy-29 <elissaralali37@gmail.com>
2026-01-23 20:34:03 +0000

    nothing important

--- src/structure/Board.java
+++ src/structure/Board.java
@@ -1,21 +1,22 @@
 package structure;
 
-public class Board{
+public class Board {
     public Stone stones[];
-    public Board(){
-        stones=new Stone[31];
-        for(int i=0;i<=30;i++){
-            stones[i]= new Stone(ColorType.NONE, i);
+
+    public Board() {
+        stones = new Stone[31];
+        for (int i = 0; i <= 30; i++) {
+            stones[i] = new Stone(ColorType.NONE, i);
         }
     }
-    public Board copy(){
-        Board newBoard=new Board();
-        for(int i=0;i<=30;i++){   
-                newBoard.stones[i] = new Stone(
-                    this.stones[i].color, 
-                    this.stones[i].position
-                );
-                newBoard.stones[i].isOut = this.stones[i].isOut;
+
+    public Board copy() {
+        Board newBoard = new Board();
+        for (int i = 0; i <= 30; i++) {
+            newBoard.stones[i] = new Stone(
+                    this.stones[i].color,
+                    this.stones[i].position);
+            newBoard.stones[i].isOut = this.stones[i].isOut;
         }
         return newBoard;
     }
--- src/structure/GameState.java
+++ src/structure/GameState.java
@@ -101,5 +101,4 @@ public class GameState{
             return "[_ , " + stone.position + "]";
         }
     }
-
 }
